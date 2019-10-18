4245cbdba8d3504f48298ee476937891c9e9276a
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-10-18 09:22:32 +0100

    got login working and token coming back

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from './components/BubblePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        App
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute path="/api/colors" component={BubblePage} />
       </div>
     </Router>
   );
 }
+
+const PrivateRoute = ({ component: Component, ...rest}) => (
+  <Route 
+    {...rest}
+    render = {props => 
+      localStorage.getItem('token')
+      ? ( <Component {...props} />)
+      : ( <Redirect to="/" /> )
+    }
+  />
+);
 
 export default App;
---
+++ client/src/axios/axiosAuth.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem('token'),
+  return axios.create({
+    header: {
+      'Content-Type': 'application/json',
+      'Authorization': `${token}`,
+    }
+  });
+};
